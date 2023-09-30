92f173b36f29bd336082f1370bba69ed42524006
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-09-30 17:45:16 +0300

    for check

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import { contactsReducer } from './ContactsSlice';
-// import { filterReducer } from './FilterSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filter: filterReducer,
-//   },
-// });
-
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './FilterSlice';
 import { contactsReducer } from './ContactsSlice';
